d6ad097664cc17236f4d042041ae9685bc184a30
Luciano <luciano_latocheski@hotmail.com>
2022-07-10 21:53:28 -0400

    Incluido opção de decodificar base64

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 
-    carregarImg = function (e) {
+    encodeImagem = function (e) {
         let reader         = new FileReader()
         fileName.innerHTML = arquivo.files[0].name
         fileSize.innerHTML = (arquivo.files[0].size / 1024 / 1024).toFixed(2) + 'Mb'
@@ -8,28 +8,46 @@ window.onload = () => {
         reader.onload = (e) => {
             preview.src = e.target.result
             txtbase.innerHTML = preview.src.split(',')[1]
+
+            if (preview.src.split(',')[0].includes("pdf"))
+                preview.src = "./pdf.jpg"
+                
         }
         reader.readAsDataURL(this.files[0])
     }
 
-    copiar = () => {
-        navigator.clipboard.writeText(txtbase.innerHTML)
+    decodeImagem = (base64) => {
+        while (previewDec.hasChildNodes()) {
+            previewDec.removeChild(previewDec.firstChild);
+        }
+
+        let embed  = document.createElement("embed")
+        
+        if (base64) {
+            let prefix = base64.includes("/9j/") ? "data:image/jpeg;base64," : "data:application/pdf;base64,"          
+            embed.src = prefix + base64 + "#toolbar=0"
+        }
+        
+        embed.classList.add("embed")
+        previewDec.appendChild(embed)
     }
 
-    zoom = (e) => {
-        e.target.classList.contains('zoom') ? e.target.classList.remove('zoom') : e.target.classList.add('zoom')
-    }
+    let btncopy    = document.getElementById('copiar')
+    let btnLimpar  = document.getElementById('limpar')
+    let btnDecode  = document.getElementById('decode')
+    let arquivo    = document.getElementById('arquivo')
+    let preview    = document.getElementById('preview')
+    let imgCode    = document.getElementById('img-base-code')
+    let previewDec = document.getElementById("preview-decode")
 
-    let btncopy  = document.getElementById('copiar')
-    let arquivo  = document.getElementById('arquivo')
-    let preview  = document.getElementById('preview')
+    let fileName   = document.getElementById('img-name')
+    let fileSize   = document.getElementById('img-size')
+    let txtbase    = document.getElementById('img-base')
 
-    let fileName = document.getElementById('img-name')
-    let fileSize = document.getElementById('img-size')
-    let txtbase  = document.getElementById('img-base')
-
-    arquivo.addEventListener("change", carregarImg)
-    preview.addEventListener("click", zoom)
-    btncopy.addEventListener("click", copiar)
+    arquivo.addEventListener("change", encodeImagem)
+    btnLimpar.addEventListener("click", () => {imgCode.value = ''})
+    btnDecode.addEventListener("click", (e) => {decodeImagem(imgCode.value)})
+    preview.addEventListener("click", (e) => decodeImagem(txtbase.textContent))
+    btncopy.addEventListener("click", () => {navigator.clipboard.writeText(txtbase.innerHTML)})
 };
 
